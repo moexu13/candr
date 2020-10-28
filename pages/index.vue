672b3dfb786833124570e32eba1e51b800f16943
Melissa Albarella <moexu13@gmail.com>
2020-10-27 18:58:06 -0700

    Built contact form per CR-2. Designed portfolio section per CR-6. The card flip animation is janky at times - figure out why later. Also refactored and renamed a few things for clarity.

--- pages/index.vue
+++ pages/index.vue
@@ -25,46 +25,121 @@
       </div>
     </header>
     <main>
-      <section class="cards">
+      <section class="menu">
         <div class="row justify-content-center m-0">
-          <div class="cards__card">
-            <span class="cards__card--text">Our Services</span>
-          </div>
-          <div class="cards__card">
-            <span class="cards__card--text">Our Work</span>
-          </div>
-          <div class="cards__card">
-            <span class="cards__card--text">Contact Us</span>
-          </div>
+          <n-link to="#services">
+            <div class="menu__menu__item mr-5">
+              <span class="menu__menu__item--text">Our Services</span>
+            </div>
+          </n-link>
+          <n-link to="#portfolio">
+            <div class="menu__menu__item mr-5">
+              <span class="menu__menu__item--text">Our Work</span>
+            </div>
+          </n-link>
+          <n-link to="#contact">
+            <div class="menu__menu__item">
+              <span class="menu__menu__item--text">Contact Us</span>
+            </div>
+          </n-link>
         </div>
       </section>
 
       <section id="main-copy" class="main-copy">
+        <h2 class="main-copy--heading">Get a Website Tailored for Your Business</h2>
+        <p class="main-copy--text">
+          Your web presence is more important than ever. Your customers expect to be able to find and do business with you online. 
+        </p>
 
-        <h2 class="main-copy--heading">Your Website Should Grow Your Business</h2>
 
+        <h3 class="main-copy--heading">Your Website Should Grow Your Business</h3>
         <p class="main-copy--text">
           Small businesses have been hit hard by the pandemic. Providing online services can help your business not only survive but thrive. 
         </p>
 
-        <h2 class="main-copy--heading">What We Offer</h2>
+        <h3 id="services" class="main-copy--heading">What We Offer</h3>
 
         <p class="main-copy--text">
           We offer a variety of services to help your business.
-        </p>
-
-        <h2 class="main-copy--heading">Get a Website Tailored for Your Business</h2>
-        
-        <p class="main-copy--text">
-          Your web presence is more important than ever. Your customers expect to be able to find and do business with you online. 
-        </p>
+        </p>      
       </section>
-
-      <section id="about"></section>
       
-      <section id="portfolio"></section>
+      <section id="portfolio" class="portfolio">
+        <h2 class="portfolio__heading">Our Work</h2>     
+        <div class="cards">
+          <div class="cards__card">
+            <div class="cards__card__face cards__card__face--front">
+              <a href="https://route53medical.com" target="_blank">
+                <img src="~assets/images/screenshots/rt53.png" alt="Route 53 Medicine" />
+              </a>
+            </div>
+            <div class="cards__card__face cards__card__face--back">
+              <a href="https://route53medical.com" target="_blank">
+                <div class="cards__card__face--back--inner">
+                  <img src="/favicon.png" alt="" />
+                  <p class="cards__card__face--back--text">
+                    Route 53 Medical
+                  </p>
+                </div>
+              </a>
+            </div>
+        </div>
+          <div class="cards__card">
+            <div class="cards__card__face cards__card__face--front">
+              <a href="https://richthetreeman.com" target="_blank">
+                <img src="~assets/images/screenshots/treeman.png" alt="The Treeman" />
+              </a>
+            </div>
+            <div class="cards__card__face cards__card__face--back">
+              <a href="https://richthetreeman.com" target="_blank">
+                <div class="cards__card__face--back--inner">
+                  <img src="/favicon.png" alt="" />
+                  <p class="cards__card__face--back--text">
+                    The Tree Man
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="cards__card">
+            <div class="cards__card__face cards__card__face--front">
+              <a href="https://terryalbarella.com" target="_blank">
+                <img src="~assets/images/screenshots/ta.png" alt="Terry Albarella" />
+              </a>
+            </div>
+            <div class="cards__card__face cards__card__face--back">
+              <a href="https://terryalbarella.com" target="_blank">
+                <div class="cards__card__face--back--inner">
+                  <img src="/favicon.png" alt="" />
+                  <p class="cards__card__face--back--text">
+                    Terry Albarella
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+          <div class="cards__card">
+            <div class="cards__card__face cards__card__face--front">
+              <a href="https://highclasstree.services" target="_blank">
+                <img src="~assets/images/screenshots/highclass.png" alt="High Class Tree Services" />
+              </a>
+            </div>
+            <div class="cards__card__face cards__card__face--back">
+              <a href="https://highclasstree.services" target="_blank">
+                <div class="cards__card__face--back--inner">
+                  <img src="/favicon.png" alt="" />
+                  <p class="cards__card__face--back--text">
+                    High Class Tree Service
+                  </p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
     
-      <section id="contact">
+      <section id="contact" class="contact-form">
+        <h2 class="contact__heading">Contact Us</h2>
         <div class="form__container">
           <form name="contact" netlify>
             <div class="form-group mt-4">
@@ -104,10 +179,25 @@
             </div>
           </form>
         </div>
-
       </section>
-
     </main>
+
+    <footer class="footer">
+      <div class="footer__social-bar">
+        <a href="https://www.facebook.com/Clicks-and-Rivets-111094736905456"
+           target="_blank"><font-awesome-icon :icon="['fab', 'facebook-f']"
+        /></a>
+        <a href="https://www.instagram.com/clicksandrivets/"
+           target="_blank"><font-awesome-icon :icon="['fab', 'instagram']"
+        /></a>
+        <a href="https://twitter.com/clicksandrivets"
+           target="_blank"><font-awesome-icon :icon="['fab', 'twitter']"
+        /></a>
+      </div>
+      <div class="footer__attribution">
+          &copy; 2020 Clicks and Rivets LLC
+      </div>
+    </footer>
   </div>
 </template>
 
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faFacebookF, faLinkedinIn, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF, faInstagram, faTwitter} from '@fortawesome/free-brands-svg-icons'
 import { faBars, faTimes } from '@fortawesome/pro-light-svg-icons'
 
 // This is important, we are going to let Nuxt.js worry about the CSS
@@ -9,7 +9,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faBars, faTimes, faFacebookF, faLinkedinIn, faTwitter)
+library.add(faBars, faTimes, faFacebookF, faInstagram, faTwitter)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
