5e0855019d1030efbf8479959fb0a2f3c480c4c4
Melissa Albarella <moexu13@gmail.com>
2020-11-03 14:01:28 -0800

    Made site responsive per CR-11. Built more business-looking retro site and added screenshot to index page per CR-12.

---
+++ pages/retro.vue
@@ -0,0 +1,156 @@
+<template>
+  <div class="retro-wrapper">
+    <h1 class="retro__header">Craterside Supply</h1>
+
+    <p class="retro__header--subtitle">Now 
+    <img src="~assets/images/retro/open-sign.jpg" alt="open sign" />
+    at our new Capital Location</p>
+
+    <!-- <div class="retro__background">&nbsp;</div> -->
+
+    <section class="retro__sidebar">
+      <h2 class="retro__sidebar--title">Weekly Specials</h2>
+        <hr />
+      <div class="retro__sidebar--item">
+        <h3 class="retro__sidebar--title"><a href="#">Compaq PC</a></h3>
+        <img src="~assets/images/retro/compaq.gif" alt="compaq pc" />
+        <ul>
+          <li class="retro--red">SALE - $899.99</li>
+          <li>(Monitor Not Included)</li>
+          <li>Presario 533Mhz - 10GB</li>
+          <li>4x CDRW - 56K</li>
+        </ul>
+      </div>
+      <hr />
+      <div class="retro__sidebar--item">
+        <h3 class="retro__sidebar--title"><a href="#">ViewSonic Monitor</a></h3>
+        <img src="~assets/images/retro/viewsonic.gif" alt="viewsonic monitor" />
+        <ul>
+          <li class="retro--red">SALE - $349.99</li>
+          <li>(Monitor Included)</li>
+          <li>19" (18" viewable) Widescreen</li>
+          <li>Up to 1600 x 1280 Resolution</li>
+        </ul>
+      </div>
+      <hr />
+      <div class="retro__sidebar--item">
+        <h3 class="retro__sidebar--title"><a href="#">Voodoo 3 3000 AGP</a></h3>
+        <img src="~assets/images/retro/voodoo3.gif" alt="3dfx voodoo 3" />
+        <ul>
+          <li class="retro--red">SALE - $79.99</li>
+          <li>(Neither PC nor Monitor Included)</li>
+          <li>High Resolution 2D/3D Accelerator</li>
+          <li>Generates 6 million triangles per second (TPS)</li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="retro__grid">
+      <div class="retro__grid--item retro__grid--item-1">
+        <img src="~assets/images/retro/motherboard.jpg" alt="motherboard" />
+        <ul>
+          <li>Gigabyte 810 Chipset 66/100Mhz FSB Socket 370 Motherboard</li>
+          <li>3PCI 0 ISA 0AGP</li>
+          <li>Onboard Sound</li>
+          <li class="retro--red">$74.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-2">
+        <ul>
+          <li><img src="~assets/images/retro/modem.jpg" alt="modem" />
+          <li>US Robotic 56K V.90 Data/Fax Modem</li>
+          <li>PCI, not Win Modem</li>
+          <li>OEM</li>
+          <li class="retro--red">$59.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-3">
+        <ul>
+          <li><img src="~assets/images/retro/ati.jpg" alt="video card" /></li>
+          <li>ATI All-in-Wonder Radeon 32MB DDR 4X AGP</li>
+          <li>TV Out</li>
+          <li>Pause Live TV</li>
+          <li>Instant Replay GUIDE Plus+</li>
+          <li class="retro--red">$199.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-4">
+        <ul>
+          <li><img src="~assets/images/retro/plex.jpg" alt="plexwriter" /></li>
+          <li>Plextor PlexWriter 12x10x32x</li>
+          <li>Internal EIDE CD-RW</li>
+          <li>2MB Internal Buffer</li>
+          <li>DRIVE ONLY</li>
+          <li class="retro--red">$174.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-5">
+        <ul>
+          <li><img src="~assets/images/retro/flash.jpg" alt="flash drives" /></li>
+          <li>Mr. Flash USA</li>
+          <li>Compact Flash Memory Card</li>
+          <li>High Speed 128MB Type I</li>
+          <li>Retail Box</li>
+          <li>ISO 9001 Certified</li>
+          <li class="retro--red">$74.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-6">
+        <ul>
+          <li><img src="~assets/images/retro/zip.gif" alt="zip drive" /></li>
+          <li>Iomega Zip 250MB USB</li>
+          <li>Ultra-thin Mobile Design</li>
+          <li>Up to 900kb/sec Transfer Rate</li>
+          <li>Compatible with both 100MB and 250MB Zip disks</li>
+          <li class="retro--red">$179.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-7">
+        <ul>
+          <li><img src="~assets/images/retro/cpu.jpg" alt="cpu" /></li>
+          <li>Intel Pentium III Processor</li>
+          <li>800 Mhz</li>
+          <li>Intel MMX Media Enhancement Technology</li>
+          <li>Dual Independent Bus Architecture</li>
+          <li>32kb Non-Blocking Cache</li>
+          <li class="retro--red">$469.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-8">
+        <ul>
+          <li><img src="~assets/images/retro/network.jpg" alt="networking kit" /></li>
+          <li>Linksys Etherfast Switched 10/100</li>
+          <li>Network in a Box</li>
+          <li>Build a Blazing Fast 10/100 Dual Speed Network</li>
+          <li>Comes with Everything You Need</li>
+          <li class="retro--red">$89.99</li>
+        </ul>
+      </div>
+      <div class="retro__grid--item retro__grid--item-9">
+        <ul>
+          <li><img src="~assets/images/retro/hd.jpg" alt="hard drive" /></li>
+          <li>Western Digital 20.5 GB Hard Drive</li>
+          <li>7200 RPM EIDE</li>
+          <li>Store all of your Internet Downloads,</li>
+          <li>Music, and Video Files</li>
+          <li class="retro--red">$166.99</li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="retro__policies">
+      <h2>SHIPPING POLICIES</h2>
+      <p class="retro__text">
+        <img src="~assets/images/retro/box.png" alt="" />
+        Due to increased Raider presence items can no longer be shipped outside Megaton.
+      </p>   
+    </section>
+
+    <footer class="retro__footer">
+      <a href="#">
+        <img src="~assets/images/retro/ie.jpg" class="retro__footer--ie" alt="best viewed with internet explorer" />
+      </a>
+      <img src="~assets/images/retro/counter.gif" class="retro__footer--counter" alt="site counter" />
+    </footer>
+  </div>
+</template>
